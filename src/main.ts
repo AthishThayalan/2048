f5aa51e28c3b7f53c9d847a7d5d07c5c0a5d0844
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-15 16:29:25 +0000

    Everytime a box is created it will add the relevant styles and value to it

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,30 @@ if (!gameBoard) {
   throw new Error("Element not found.");
 }
 
-const updateBox = (): void => {
-  console.log("hi");
+const updateBoard = (box: any, value: number): void => {
+  box.innerText = "";
+  box.classList.value = "";
+  box.classList.add("board__box");
+  if (value !== 0) {
+    box.innerText = value;
+    box.classList.add(`board__box--${value}`);
+  }
 };
 
 const loadGame = (): void => {
   let board: number[][] = [
-    [0, 2, 0, 0],
-    [0, 0, 0, 0],
-    [0, 2, 0, 0],
+    [2, 0, 0, 0],
+    [0, 4, 16, 0],
+    [0, 8, 32, 0],
     [0, 0, 0, 0],
   ];
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       let box = document.createElement("div");
       box.classList.add("board__box");
-      box.classList.add("board__box--64");
-      box.innerText = board[i][j].toString();
+      box.id = i.toString() + "-" + j.toString();
+      let value: number = board[i][j];
+      updateBoard(box, value);
       gameBoard.appendChild(box);
     }
   }
