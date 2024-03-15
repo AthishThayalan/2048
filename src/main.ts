63e3dd40a9020127ff2e4b615cd4bef9aa7327c9
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-15 16:05:52 +0000

    Manually changed width so boxes fill up entire board

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,23 @@ if (!gameBoard) {
   throw new Error("Element not found.");
 }
 
+const updateBox = (): void => {
+  console.log("hi");
+};
+
 const loadGame = (): void => {
   let board: number[][] = [
-    [2, 0, 4, 8],
-    [2, 0, 4, 8],
-    [2, 0, 4, 8],
-    [2, 0, 4, 8],
+    [0, 2, 0, 0],
+    [0, 0, 0, 0],
+    [0, 2, 0, 0],
+    [0, 0, 0, 0],
   ];
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       let box = document.createElement("div");
       box.classList.add("board__box");
+      box.classList.add("board__box--64");
       box.innerText = board[i][j].toString();
-      box.classList.add("twoThousandFortyEight");
       gameBoard.appendChild(box);
     }
   }
