780524f44d126c6bcb4b8995572c94c95cd366c3
Athish Thayalan <tathish@hotmail.co.uk>
2024-03-18 08:30:23 +0000

    Trying to make the array shift to the left. Bugs in function for now .

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ const gameBoard = document.querySelector<HTMLDivElement>(".board");
 if (!gameBoard) {
   throw new Error("Element not found.");
 }
+let board: number[][] = [
+  [2, 2, 0, 0],
+  [0, 4, 16, 0],
+  [0, 8, 32, 0],
+  [0, 0, 0, 0],
+];
 
 const updateBoard = (box: any, value: number): void => {
   box.innerText = "";
@@ -16,17 +22,11 @@ const updateBoard = (box: any, value: number): void => {
 };
 
 const loadGame = (): void => {
-  let board: number[][] = [
-    [2, 0, 0, 0],
-    [0, 4, 16, 0],
-    [0, 8, 32, 0],
-    [0, 0, 0, 0],
-  ];
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       let box = document.createElement("div");
       box.classList.add("board__box");
-      box.id = i.toString() + "-" + j.toString();
+      box.id = i.toString() + "-" + j.toString(); // keep track of box position. Might be easier to move in x and y direction
       let value: number = board[i][j];
       updateBoard(box, value);
       gameBoard.appendChild(box);
@@ -34,4 +34,29 @@ const loadGame = (): void => {
   }
 };
 
+const handleLeftPress = (): void => {
+  console.log("pressed");
+  let returnArr: number[] = [];
+  for (let i = 0; i < 4; i++) {
+    let removeZero = board[i].filter((num) => num !== 0);
+    for (let j = 0; j < 4; j++) {
+      if (removeZero[j] === removeZero[j + 1]) {
+        removeZero[j] += removeZero[j + 1];
+        removeZero[j + 1] = 0;
+      }
+    }
+    let removeZeroAgain: number[] = removeZero.filter((num) => num !== 0);
+    while (removeZeroAgain.length < 4) {
+      removeZeroAgain.push(0);
+    }
+    board[i] = removeZeroAgain;
+    console.log(removeZeroAgain);
+  }
+};
+
 document.addEventListener("DOMContentLoaded", loadGame);
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    handleLeftPress();
+  }
+});
